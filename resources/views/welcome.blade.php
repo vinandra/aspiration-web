7355a84727824c53de7b45ccc634af5f531c0d61
vinandra <vinandra207@gmail.com>
2025-05-05 18:09:25 +0700

    feat : fixing styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,27 +21,27 @@
     @include('layouts.navbar1')
 
     <!-- Carousel Section -->
-    <div id="carouselExampleAutoplay" class="carousel slide mt-5" data-bs-ride="carousel">
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="{{ asset('images/carousel/gambar1.jpg') }}" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-          <img src="{{ asset('images/carousel/gambar2.jpg') }}" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-          <img src="{{ asset('images/carousel/gambar3.jpg') }}" class="d-block w-100" alt="...">
-        </div>
+  <div id="carouselExampleAutoplay" class="carousel slide mt-5" data-bs-ride="carousel" data-bs-interval="3000">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="{{ asset('images/carousel/gambar1.jpg') }}" class="d-block w-100" alt="...">
       </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplay" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplay" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
+      <div class="carousel-item">
+        <img src="{{ asset('images/carousel/gambar2.jpg') }}" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="{{ asset('images/carousel/gambar3.jpg') }}" class="d-block w-100" alt="...">
+      </div>
     </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplay" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplay" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
 
     <!-- Konten Utama -->
     <main class="container my-5">
